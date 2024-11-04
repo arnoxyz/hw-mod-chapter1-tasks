24bb58364ff5da2bcc374d862b48ebfce0b59332
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2024-11-03 19:30:04 -0500

    binary 2 decimal working, start decimal to bcd

--- bin2dec/src/bin2dec.vhd
+++ bin2dec/src/bin2dec.vhd
@@ -31,14 +31,37 @@ begin
 		decRange <= local_dec;
 	end process;
 
-
-	--TODO:
 	--convert dec to bcd
-	--conversion2 : process(bin_in)
-	-- begin 
-	-- 	for i in dec_out'range loop
-	-- 		bcd_out <= bin_in;
-	-- 	end loop;
-	-- end process;
+	conversion2 : process(decRange)
+		function to_bcd(a : integer) return std_ulogic_vector is 
+		begin 
+			case a is
+				when 1 => return "0001";
+				when 2 => return "0010";
+				when 3 => return "0011";
+				when 4 => return "0100";
+				when 5 => return "0101";
+				when 6 => return "0110";
+				when 7 => return "0111";
+				when 8 => return "1000";
+				when 9 => return "1001";
+				when others => return "1111";
+			end case;
 
+			return "1111";
+		end function;
+
+		variable size : integer := (log10c(decRange)*4)-1;
+		--variable local_bcd : std_ulogic_vector((log10c(decRange)*4)-1 downto 0);
+		variable local_bcd : std_ulogic_vector(7 downto 0);
+	begin 
+		for i in 0 to log10c(decRange) loop
+			if i = log10c(decRange) then 
+				local_bcd(3 downto 0) := to_bcd(decRange mod 10);
+			else 
+				local_bcd(7 downto 4) := to_bcd(decRange/(10**i));
+			end if;
+		end loop;
+		bcd_out <= local_bcd;
+	end process;
 end architecture;
--- bin2dec/tb/bin2dec_tb.vhd
+++ bin2dec/tb/bin2dec_tb.vhd
@@ -14,7 +14,7 @@ architecture tb of bin2dec_tb is
 	signal bin_in : std_ulogic_vector(width-1 downto 0);
 	signal dec_out : integer;
 
-	signal bcd_out : std_ulogic_vector(3 downto 0);
+	signal bcd_out : std_ulogic_vector(7 downto 0);
 begin
 	uut : bin2dec
 	port map(
@@ -24,15 +24,25 @@ begin
 	);
 
 	stimuli : process
+		procedure test1 is 
+		begin 
+			for i in 2**width-1 downto 0 loop
+				bin_in <= std_ulogic_vector(to_unsigned(i, width));
+				wait for 1 ns;
+				assert dec_out = to_integer(unsigned(bin_in));
+			end loop;
+		end procedure;
+
 	begin
 		-- apply your stimulus here
-		-- This is just a template - adjust to your needs!
-		-- report to_string(bin_in) & " is decimal:" & to_string(dec_out) & " is BCD " & to_string(bcd_out);
-		for i in 2**width-1 downto 0 loop
-			bin_in <= std_ulogic_vector(to_unsigned(i, width));
-			wait for 1 ns;
-			assert dec_out = to_integer(unsigned(bin_in));
-		end loop;
+		--test1;
+
+		bin_in <= "1111";
+		wait for 1 ns;
+		bin_in <= "0111";
+		wait for 1 ns;
+
+
 		--report to_string(dec_out) & " " & to_string(bcd_out);
 		wait;
 	end process;
