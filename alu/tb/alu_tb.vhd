a5b904940e639b2d32489da142dde1921f2833de
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2024-11-04 16:48:31 -0500

    fixed alu tb

--- alu/tb/alu_tb.vhd
+++ alu/tb/alu_tb.vhd
@@ -13,8 +13,8 @@ architecture tb of alu_tb is
 	constant DATA_WIDTH : positive := 32;
 	constant x : natural := log2c(DATA_WIDTH);
 	signal op : alu_op_t;
-	signal a : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (0=>'1', others=>'0');
-	signal b : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (0=>'1', others=>'0');
+	signal a : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (0=>'1', 2=>'1', others=>'0');
+	signal b : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (0=>'1', 3=>'1', others=>'0');
 	signal r : std_ulogic_vector(DATA_WIDTH-1 downto 0);
 	signal z : std_ulogic;
 begin
@@ -69,6 +69,7 @@ begin
 		end if;
 
 		op <= ALU_SLTU;
+		wait for 1 ns;
 		if signed(a) < signed(b) then 
 			assert r(0) = '1' report "ALU_SLT error" severity error;
 		else 
@@ -79,7 +80,9 @@ begin
 		op <= ALU_SLL;
 		wait for 1 ns;
 		assert r = std_ulogic_vector(shift_left(unsigned(A), to_integer(unsigned(B(x downto 0))))) report "ALU_SLL error" severity error;
-		
+
+		A <= (DATA_WIDTH-1 => '1', DATA_WIDTH-2 => '1', others=>'0');
+		wait for 1 ns;
 
 		op <= ALU_SRL;
 		wait for 1 ns;
